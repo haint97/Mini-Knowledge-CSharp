012a268d4303c27e9b701424c3cc91056947e85b
haint97 <nguyenthaihai00@gmail.com>
2020-02-16 15:57:50 +0700

    Observer pattern

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesignPattern.Behavioral_Patterns;
+using System;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Observer.RunDemo();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
