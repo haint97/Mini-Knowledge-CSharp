bea90e62092943c2d8a6a9b9e9f7373e42ec11cf
haint97 <nguyenthaihai00@gmail.com>
2020-02-18 14:33:55 +0700

    AbstractFactory

---
+++ DesignPattern/Behavioral Patterns/Strategy.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Behavioral_Patterns
+{
+   // allow a group of  algorithms, encapsulate each one, 
+    //and make them interchangeable.Strategy lets the algorithm vary independently from clients that use it
+    class Strategy
+    {
+    }
+    public class Student
+    {
+        public Student(Student st)
+        {
+            Age = st.Age;
+            Id = st.Id;
+            Mark = st.Mark;
+            Name = st.Name;
+        }
+
+        public Student()
+        {
+        }
+
+        private SortStrategy _sortstrategy;
+
+         void SetSortStrategy(SortStrategy sortstrategy)
+        {
+            this._sortstrategy = sortstrategy;
+        }
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Mark { get; set; }
+    }
+
+   abstract class SortStrategy
+    {
+        public abstract void Sort(Student[] students);
+    }
+
+    public class Container
+    {
+        public Container()
+        {
+            Students = new Student[3];
+            Students[0] = new Student
+            {
+                Age = 1,
+                Id = 1,
+                Mark = 1,
+                Name = "a"
+            };
+            Students[1] = new Student
+            {
+                Age = 3,
+                Id = 3,
+                Mark = 3,
+                Name = "c"
+            };
+            Students[2] = new Student
+            {
+                Age = 2,
+                Id = 2,
+                Mark = 2,
+                Name = "b"
+            };
+        }
+
+        public Student[] Students { get; set; }
+
+        public void OutputResult()
+        {
+            for (int i = 0; i < Students.Length; i++)
+            {
+                Console.WriteLine("-----" + (i + 1) + "----");
+                Console.WriteLine("Id  " + Students[i].Id);
+                Console.WriteLine("Name  " + Students[i].Name);
+                Console.WriteLine("Mark  " + Students[i].Mark);
+                Console.WriteLine("Age  " + Students[i].Age);
+            }
+        }
+
+
+    }
+
+    class AgeSortStrategy : SortStrategy
+    {
+        public override void Sort(Student[] students)
+        {
+            Console.WriteLine("sorted by age");
+        }
+    }
+    class MarkSortStrategy : SortStrategy
+    {
+        public override void Sort(Student[] students)
+        {
+            Console.WriteLine("sorted by mark");
+        }
+    }
+
+}
---
+++ DesignPattern/Creational Patterns/AbstractFactory.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Creational_Patterns
+{
+    // creating families of related or dependent objects without specifying their concrete classes.
+    class AbstractFactory
+    {
+        public static void RunDemo()
+        {
+
+
+            TransportFactory yamahaFactory = new YamahaFactory();
+            TransportWorld world = new TransportWorld(yamahaFactory);
+            world.Produce();
+
+            TransportFactory hondaFactory = new HondaFactory();
+             world = new TransportWorld(hondaFactory);
+            world.Produce();
+
+
+        }
+    }
+
+    abstract class Bike
+    {
+
+    }
+    abstract class MotorBike
+    {
+        public abstract void Specification();
+    }
+
+    abstract class TransportFactory
+    {
+        public abstract Bike ProduceBike();
+        public abstract MotorBike ProduceMotorbike();
+
+    }
+
+     class HondaFactory : TransportFactory
+    {
+        public override Bike ProduceBike()
+        {
+            return new HondaBike();
+        }
+
+        public override MotorBike ProduceMotorbike()
+        {
+            new HondaMotorBike();
+        }
+    }
+    class YamahaFactory : TransportFactory
+    {
+        public override Bike ProduceBike()
+        {
+            return new YamahaBike();
+        }
+
+        public override MotorBike ProduceMotorbike()
+        {
+           return new YamahaMotorBike();
+        }
+    }
+
+
+    class HondaBike : Bike
+    {
+
+    }
+    class YamahaBike : Bike
+    {
+
+    }
+
+
+    class HondaMotorBike : MotorBike
+    {
+        public override void Specification()
+        {
+            Console.WriteLine(this.GetType().Name +
+        " belong " + " Honda");
+        }
+    }
+    class YamahaMotorBike : MotorBike
+    {
+        public override void Specification()
+        {
+            Console.WriteLine(this.GetType().Name +
+       " belong " + " yamaha");
+        }
+    }
+
+
+    /// <summary>
+    /// The 'Client' class 
+    /// </summary>
+
+    class TransportWorld
+
+    {
+        Bike _bike;
+        MotorBike _motorBike;
+
+        // Constructor
+
+        public TransportWorld(TransportFactory factory)
+        {
+            _bike = factory.ProduceBike();
+            _motorBike = factory.ProduceMotorbike();
+        }
+
+        public void Produce()
+        {
+            _bike.ToString();
+            _motorBike.ToString();
+        }
+
+    }
+}
